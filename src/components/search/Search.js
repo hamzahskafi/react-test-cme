0aab927f593810c1c41995e87faa9c979ec1d322
hamzahskafi <24636307+hamzahskafi@users.noreply.github.com>
2020-10-14 02:16:10 +0300

    added api request and base list model

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar'
+import youtube from '../../api/youtube.js'
+import VideoList from './VideoList'
+
 class Search extends Component{
-    
+
+    state = {
+        videos: []
+    }
+    handleSubmit = async (termFromSearchBar) => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: termFromSearchBar
+            }
+        })
+
+        this.setState({
+            videos: response.data.items
+        })
+    };
+
 
     render() {
         return (
-            <div>
-                <SearchBar/>
+            <div className="main_container">
+                <SearchBar handleFormSubmit={this.handleSubmit}/>
+                <div className="video_list">
+                            <VideoList videos={this.state.videos}/>
+                    </div>
             </div>
         )
     }
---
+++ src/components/search/VideoList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const VideoList = (videos) => {
+    const VideosList = null;
+
+    return <div className='compiled_videos_list'>{VideosList}</div>;
+};
+export default VideoList;
